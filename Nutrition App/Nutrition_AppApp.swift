5d4ba86a8085e361e4438725d41c888ffe32f397
pmcslarrow <pmcslarrow@icloud.com>
2023-02-12 20:12:22 -0800

    Initial Commit

---
+++ Nutrition App/Nutrition_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nutrition_AppApp.swift
+//  Nutrition App
+//
+//  Created by Paul McSlarrow on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Nutrition_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
